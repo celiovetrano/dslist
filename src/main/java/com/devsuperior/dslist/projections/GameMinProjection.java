78e9907bb9c23cebe6046bc8e6ddb9f869aa439f
Celio Vetrano <celio.vetrano@gmail.com>
2025-01-25 19:18:12 -0300

    Homolog OK

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 	
 	Long getId();
 	String getTitle();
-	Integer getByYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
